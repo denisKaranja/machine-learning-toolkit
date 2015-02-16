75c41b0cced72236b3498066420fd0388da4b0f4
Denis Karanja <deebeat@students.uonbi.ac.ke>
2015-02-16 20:39:17 +0300

    K means CLustering complete(k_means_cluster.py)

--- k_means_cluster.py
+++ k_means_cluster.py
@@ -6,7 +6,7 @@ Date: 15th Feb, 2015 10:31pm
 __author__ = "Denis Karanja"
 __version__ = "1.0.0"
 
-import random
+import random, fractions
 
 def euclidean_distance(point, centroid):
 	'''Returns the euclidean distance between two points'''
@@ -38,8 +38,46 @@ def get_coordinates(points):
 
 	return coordinates
 
+def get_group_matrix(coords, centroid_one, centroid_two):
+	'''Returns a group matrix'''
+	euclid_distance = []
+	grp_matrix = []
 
-def group_matrix(points):
+	for y in coords:
+		#get distance for each point in regard to centroid one
+		distance_one = euclidean_distance(y, centroid_one)
+
+		#get distance for each point in regard to centroid two
+		distance_two = euclidean_distance(y, centroid_two)
+
+		euclid_distance.append((distance_one, distance_two))
+
+		#group matrix condtions
+		if distance_one > distance_two:
+			grp_matrix.append((0, 1))
+		elif distance_one < distance_two:
+			grp_matrix.append((1, 0))
+
+	return grp_matrix
+
+def get_avg_centroid(x_y_index, coords):
+	'''Returns new centroid coordinates if more than 1 point eppears in any cluster'''
+	x_coords, y_coords = [], []
+	for index in x_y_index:
+		#new_points.append(coords[index])
+		x, y = coords[index]
+		x_coords.append(x)
+		y_coords.append(y)
+
+	#get average of both x and y coords
+	x_coords = round(sum(x_coords) / (len(x_coords) * 1.0), 4)
+	y_coords = round(sum(y_coords) / (len(y_coords) * 1.0), 4)
+
+	centroid = (x_coords, y_coords)
+
+	return centroid
+
+def next_group_matrix(points):
 	'''Return the group matrix given coordinates'''
 	coords = get_coordinates(points)
 	centroids = []
@@ -59,32 +97,67 @@ def group_matrix(points):
 	#get the centroids as per the above rand positions
 	centroids = tuple(centroids)
 	i, j = centroids
-	centroid_one = coords[i]
-	centroid_two = coords[j]
+	centroid_one = (1,1)#coords[i]
+	centroid_two = (2, 1)#coords[j]
 
-	#get euclidean distance for the points and the centroids
-	for y in coords:
-		#get distance for each point in regard to centroid one
-		distance_one = euclidean_distance(y, centroid_one)
+	#get the group matrix
+	grp_matrix = get_group_matrix(coords, centroid_one, centroid_two)
 
-		#get distance for each point in regard to centroid two
-		distance_two = euclidean_distance(y, centroid_two)
+	while True:
+		#iterate till group matrix is stable
 
-		euclid_distance.append((distance_one, distance_two))
+		#get the number of points in each cluster
+		a, b, m_index_values, n_index_values = [], [], [], []
+		for index, x_y_values in enumerate(grp_matrix):
+			m, n = x_y_values
+			a.append(m)
+			b.append(n)
 
-		#group matrix condtions
-		if distance_one > distance_two:
-			grp_matrix.append((0, 1))
-		elif distance_one < distance_two:
-			grp_matrix.append((1, 0))
+			if m == 1:
+				m_index_values.append(index)
+			elif n == 1:
+				n_index_values.append(index)
 
-	return grp_matrix
+		cluster_one_elems = sum(a)
+		cluster_two_elems = sum(b)
+
+		if cluster_one_elems == 1:
+			#use the same centroid from the previous one
+			centroid_one = centroid_one
+
+		elif cluster_one_elems > 1:
+			#new centroid is the average of the elements
+			centroid_one = get_avg_centroid(m_index_values, coords)
+
+		if cluster_two_elems == 1:
+			#use the same centroid used in the last iteration
+			centroid_two = centroid_two
+
+		elif cluster_two_elems > 1:
+			#new centroid is the average of the elements
+			centroid_two= get_avg_centroid(n_index_values, coords)
+		
+		#print(centroid_one, centroid_two)
+
+		#get new group matrix
+		new_grp_matrix = get_group_matrix(coords, centroid_one, centroid_two)
+
+		#when no more change happens, stop iteration
+		if new_grp_matrix == grp_matrix:
+			return grp_matrix
+
+		grp_matrix = new_grp_matrix
+
 
 def k_means_cluster(points):
 	'''Returns the specific clusters where points lie'''
 	#get the group matrix
-	grp_matrix = group_matrix(points)
+	grp_matrix, centroids, coords = group_matrix(points)
 	#get the number of elements in each cluster
+
+	#previous centroids
+	centroid_one, centroid_two = centroids
+	
 	a, b = [], []
 	for x_y_values in grp_matrix:
 		m, n = x_y_values
@@ -99,14 +172,16 @@ def k_means_cluster(points):
 	while True:
 		if cluster_one_elems == 1:
 			#use the same centroid from the previous one
-			pass
+			centroid_one = centroid_one
+
 		elif cluster_one_elems > 1:
 			#new centroid is the average of the elements
 			pass
 
 		if cluster_two_elems == 1:
 			#use the same centroid used in the last iteration
-			pass
+			centroid_two = centroid_two
+
 		elif cluster_two_elems > 1:
 			#new centroid is the average of the elements
 			pass
@@ -115,10 +190,10 @@ def k_means_cluster(points):
 		#when no more change happens, stop iteration
 		if grp_matrix == temp_grp_matrix:
 			return grp_matrix
-
+	
 
 	#return euclid_distance, grp_matrix
 
 
 if __name__ == "__main__":
-	print(group_matrix(4))
+	print(next_group_matrix(4))
