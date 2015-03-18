dad782d85e105ec465dc6f00f8498a2f55eb3976
Denis Karanja <deebeat@students.uonbi.ac.ke>
2015-03-18 09:47:50 +0300

    Clean ups into folders

---
+++ Q-Learning/q_learning.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Task: Q learning algorithm for machine learning...
+
+Date: 18th Feb, 2015 8:35 (pm)
+"""
+
+__author__ = "Denis Karanja"
+__version__ = "1.0.0"
+
+def q_matrix():
+	pass
+
+def r_matrix():
+	pass
+
+def q_learning():
+	pass
+
+
